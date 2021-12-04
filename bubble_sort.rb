ad75371f7d8ca89dc0b10e9b33054cdd40e04edf
wilpoly <polype@pm.me>
2021-12-04 20:45:54 +0100

    Add optimization

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,19 +2,23 @@ def bubble_sort(array)
   swapped = false
   k = array.length
   loop do
-    swapped = false
+    # swapped = false
+    new_k = 0
     for i in 1...k do
       if array[i-1] > array[i]
         temp = array[i-1]
         array[i-1] = array[i]
         array[i] = temp
-        swapped = true
+        # swapped = true
+        new_k = i
         puts k
         p array
       end
     end
-    k -= 1
-    break if swapped == false
+    k = new_k
+    # k -= 1
+    # break if swapped == false
+    break if k <= 1
   end
   array
 end
